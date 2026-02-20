0a036cd9623342b63a6ea45b79f88a95cfe30a92
@stevo.juhas <64430078+Stefan-creator-arch@users.noreply.github.com>
2026-02-20 21:38:15 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -28,3 +28,26 @@ messaging.onBackgroundMessage((payload) => {
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAdhUJoq7szgFbp8WN5bTKx3Ib6OR8Oh5o",
+  authDomain: "stefanov-chat.firebaseapp.com",
+  projectId: "stefanov-chat",
+  messagingSenderId: "915409159507",
+  appId: "1:915409159507:web:6150b7b7b91e665d68cda6"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Notifikácia na pozadí:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/static/icon.png' // ak máš ikonku, zmeň cestu
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
